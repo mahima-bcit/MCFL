b32c9f798a076603d1ea73cd8ec2d5bb92a51c40
Mahima Sharma - BCIT <msharma163@my.bcit.ca>
2026-04-23 21:02:54 -0700

    refactor: remove Navbar and Footer from Index page to fix duplicated header and footer

--- Frontend/src/pages/Index.tsx
+++ Frontend/src/pages/Index.tsx
@@ -1,17 +1,13 @@
-import Navbar from "../components/layout/Navbar";
-import Footer from "../components/layout/Footer";
 import Hero from "../components/sections/Hero";
 import HowItWorks from "../components/sections/HowItWorks";
 
 export default function Index() {
   return (
     <div className="min-h-screen bg-mint font-body">
-      <Navbar />
       <main>
         <Hero />
         <HowItWorks />
       </main>
-      <Footer />
     </div>
   );
 }
