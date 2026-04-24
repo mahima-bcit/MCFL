9a3738ef2d04d0d46c9545fcf515bf69b8f06c63
amritsagoo91 <amritsagoo91@gmail.com>
2026-04-24 14:41:52 -0700

    parentFeedback Backend

---
+++ Backend/MCFL.API/DTOs/ParentFeedbackRequestDto.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MCFL.API.Models.DTOs;
+
+public class ParentFeedbackRequestDto
+{
+    [Required]
+    [MaxLength(255)]
+    [EmailAddress]
+    public string ParentEmail { get; set; } = null!;
+
+    [MaxLength(100)]
+    public string? ParentName { get; set; }
+
+    [Required]
+    public string MoneyStory { get; set; } = null!;
+
+    [Required]
+    public string WhatChildShouldLearn { get; set; } = null!;
+
+    [Required]
+    public string Token { get; set; } = null!;
+}
